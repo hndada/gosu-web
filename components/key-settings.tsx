61e2bcbe7f4f33e7cbacd422d58763c682c58e67
Muang <hndada@dgist.ac.kr>
2022-12-30 15:06:49 +0900

    모드 바뀌어도 마지막 키세팅 유지

--- components/key-settings.tsx
+++ components/key-settings.tsx
@@ -21,27 +21,42 @@ const style = css`
   border-color: darkcyan;
 }
 `
-export default function KeySettings({ mode, defaultKeySettings }) {
-  const [keySettings, setKeySettings] = useState(defaultKeySettings); // useState(['D', 'F', 'J', 'K']);
-  const [cursor, setCursor] = useState<number>(keySettings.length);
+export default function KeySettings({ mode }) {
+  const [piano4, setPiano4] = useState(['d', 'f', 'j', 'k']);
+  const [piano7, setPiano7] = useState(['s', 'd', 'f', ' ', 'j', 'k', 'l']);
+  const [drum4, setDrum4] = useState(['z', 'x', 'c', 'v']);
+  const [cursor, setCursor] = useState<number>(10);
+
   let maxKeyLength = mode === 1 ? 7 : 4;
-  // console.log(mode, maxKeyLength);
-  // onClick되면 cursor = 0, keySettings 비우기
-  // cursor <  length 면 keydown 들어온 키 이름을 cursor 에 입력, cursor++
-  // cursor >= length 되면 인풋 안 받음
-
-  useEffect(() => {
-    setKeySettings(defaultKeySettings);
-  }, [defaultKeySettings]);
-
   useEffect(() => {
     const handleKeydown = (event) => {
       if (cursor >= maxKeyLength) { return }
-      setKeySettings(prev => {
-        const newKeySettings = [...prev];
-        newKeySettings[cursor] = event.key;
-        return newKeySettings
-      });
+      // let set =
+      //   mode === 0 ? setPiano4 :
+      //     mode === 1 ? setPiano7 :
+      //       mode === 2 ? setDrum4 :
+      //         null;
+      if (mode === 0) {
+        setPiano4(prev => {
+          const newKeySettings = [...prev];
+          newKeySettings[cursor] = event.key;
+          return newKeySettings
+        });
+      }
+      if (mode === 1) {
+        setPiano7(prev => {
+          const newKeySettings = [...prev];
+          newKeySettings[cursor] = event.key;
+          return newKeySettings
+        });
+      }
+      if (mode === 2) {
+        setDrum4(prev => {
+          const newKeySettings = [...prev];
+          newKeySettings[cursor] = event.key;
+          return newKeySettings
+        });
+      }
       setCursor(prev => prev + 1) // 함수가 인자로 들어오면 prev가 getter로 동작
     };
     window.addEventListener('keydown', handleKeydown);
@@ -50,16 +65,75 @@ export default function KeySettings({ mode, defaultKeySettings }) {
     };
   }, [cursor]);
 
+  // let keySettings = [piano4, piano7, drum4][mode];
+  // let setKeySettings [setPiano4, setPiano7, setDrum4][mode];
+
   return <div css={style} onClick={() => {
     setCursor(0);
-    setKeySettings(Array(keySettings.length).fill(undefined));
+    {
+      mode === 0 ? Array(4).fill(undefined) :
+        mode === 1 ? Array(7).fill(undefined) :
+          mode === 2 ? Array(4).fill(undefined)
+            : null
+    }
   }}>
-    {Array(keySettings.length).fill(0).map((_, i) => (
+    {Array(maxKeyLength).fill(0).map((_, i) => (
       <div className="key" style={cursor === i ? { backgroundColor: "yellow" } : undefined}>
-        {keySettings[i]}
+        {mode === 0 ? piano4[i].toUpperCase()
+          : mode === 1 ? piano7[i].toUpperCase()
+            : mode === 2 ? drum4[i].toUpperCase()
+              : null}
       </div>
     ))}
   </div>
+
+
+
+  // let defaultKeySettingsSet = [
+  //   ['d', 'f', 'j', 'k'],
+  //   ['s', 'd', 'f', ' ', 'j', 'k', 'l'],
+  //   ['z', 'x', 'c', 'v']
+  // ]
+
+
+  // const [keySettings, setKeySettings] = useState(defaultKeySettings); // useState(['D', 'F', 'J', 'K']);
+  // const [cursor, setCursor] = useState<number>(keySettings.length);
+  // let maxKeyLength = mode === 1 ? 7 : 4;
+  // // console.log(mode, maxKeyLength);
+  // // onClick되면 cursor = 0, keySettings 비우기
+  // // cursor <  length 면 keydown 들어온 키 이름을 cursor 에 입력, cursor++
+  // // cursor >= length 되면 인풋 안 받음
+
+  // useEffect(() => {
+  //   setKeySettings(defaultKeySettings);
+  // }, [defaultKeySettings]);
+
+  // useEffect(() => {
+  //   const handleKeydown = (event) => {
+  //     if (cursor >= maxKeyLength) { return }
+  //     setKeySettings(prev => {
+  //       const newKeySettings = [...prev];
+  //       newKeySettings[cursor] = event.key;
+  //       return newKeySettings
+  //     });
+  //     setCursor(prev => prev + 1) // 함수가 인자로 들어오면 prev가 getter로 동작
+  //   };
+  //   window.addEventListener('keydown', handleKeydown);
+  //   return () => {
+  //     window.removeEventListener('keydown', handleKeydown);
+  //   };
+  // }, [cursor]);
+
+  // return <div css={style} onClick={() => {
+  //   setCursor(0);
+  //   setKeySettings(Array(keySettings.length).fill(undefined));
+  // }}>
+  //   {Array(keySettings.length).fill(0).map((_, i) => (
+  //     <div className="key" style={cursor === i ? { backgroundColor: "yellow" } : undefined}>
+  //       {keySettings[i]}
+  //     </div>
+  //   ))}
+  // </div>
 }
 
 // https://www.sliderrevolution.com/resources/css-range-slider/
