8535e31f3f4d966eb260e978220f0b1cdb883c23
Muang <hndada@dgist.ac.kr>
2023-01-01 15:36:30 +0900

    remove old commented code

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,3 @@ import Choice from './choice'
 export default function IndexPage() {
   return <Choice />
 }
-{/* <Router >
-<Routes>
-  <Route path="/" element={<Choice />} />
-  <Route path="/play" element={<Play />} />
-  <Route path="*" element={<Choice />} />
-</Routes>
-</Router> */}
